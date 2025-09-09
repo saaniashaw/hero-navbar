559dea76e8a985acdbf7c285d7634e8c8d91deab
saaniashaw <106764211+saaniashaw@users.noreply.github.com>
2025-09-09 20:57:57 +0530

    Add files via upload

---
+++ components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Us</h2>
+      <p>
+        We are a team of passionate designers and developers who love bringing
+        ideas to life. Our mission is to deliver creative, impactful, and
+        user-friendly digital experiences.
+      </p>
+    </section>
+  );
+}
+
+export default About;
